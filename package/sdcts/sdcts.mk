2698578ed43fa8ceb1d21c0b018618ab7d3a662c
Dimitrios Siganos <dimitris@siganos.org>
2015-07-13 12:34:49 +0100

    Integrate Timespace sdcts package into buildroot

---
+++ package/sdcts/sdcts.mk
@@ -0,0 +1,17 @@
+SDCTS_VERSION = master
+SDCTS_SITE = https://github.com/dsiganos/tspace_active_antenna.git
+SDCTS_SITE_METHOD = git
+#SDCTS_SITE_DEPENDENCIES =
+
+define SDCTS_FIXUP_SOURCES
+	rm -r $(@D)/{README.md,laird_releases,doc,images}
+	rm $(@D)/sdcts/sdcts
+	mv $(@D)/sdcts $(@D)/sdcts.tmpdir
+	mv $(@D)/sdcts.tmpdir/.deps $(@D)
+	mv $(@D)/sdcts.tmpdir/* $(@D)
+	rmdir $(@D)/sdcts.tmpdir
+endef
+
+SDCTS_POST_EXTRACT_HOOKS += SDCTS_FIXUP_SOURCES
+
+$(eval $(autotools-package))
