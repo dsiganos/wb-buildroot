d249996ca21a33a98f2b27334e2d1bccd1b1abac
Dimitrios Siganos <dimitris@siganos.org>
2015-09-06 12:16:44 +0100

    Compile sdcts from the local directory maintained by repo
    sdcts is now checkout by repo at $(TOPDIR)/../../sdcts
    sdcts project needs autoreconfiguring

--- package/sdcts/sdcts.mk
+++ package/sdcts/sdcts.mk
@@ -1,17 +1,7 @@
-SDCTS_VERSION = a4b847a35fe4715b01deaee11420240562ff5047
-SDCTS_SITE = https://github.com/dsiganos/tspace_active_antenna.git
-SDCTS_SITE_METHOD = git
+SDCTS_VERSION = custom
+SDCTS_SITE = $(TOPDIR)/../../sdcts
+SDCTS_SITE_METHOD = local
+SDCTS_AUTORECONF = YES
 #SDCTS_SITE_DEPENDENCIES =
-
-define SDCTS_FIXUP_SOURCES
-	rm -r $(@D)/{README.md,laird_releases,doc,images}
-	rm $(@D)/sdcts/sdcts
-	mv $(@D)/sdcts $(@D)/sdcts.tmpdir
-	mv $(@D)/sdcts.tmpdir/.deps $(@D)
-	mv $(@D)/sdcts.tmpdir/* $(@D)
-	rmdir $(@D)/sdcts.tmpdir
-endef
-
-SDCTS_POST_EXTRACT_HOOKS += SDCTS_FIXUP_SOURCES
 
 $(eval $(autotools-package))
