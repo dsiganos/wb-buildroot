a4c78e911f5e38180e13ca0847cc0aaf79157281
Dimitrios Siganos <dimitris@siganos.org>
2015-11-10 04:54:25 +0000

    Disable and remove the lighttpd web server

--- board/timespace/active_antenna/post_build.sh
+++ board/timespace/active_antenna/post_build.sh
@@ -6,4 +6,8 @@ regexp='s/^AC_INIT(\[sdcts\], \[\(.*\)\])[ \t]*$/\1/p'
 ver=`sed -n -e "$regexp" ../../sdcts/configure.ac`
 echo "Timespace Active Antenna $ver" > $TARGETDIR/etc/timespace-release
 
+# remove the web server init scripts
+rm -f $TARGETDIR/etc/init.d/S??lighttpd
+rm -f $TARGETDIR/usr/sbin/lighty
+
 echo "active antenna POST BUILD script: done."
