2972e0c8efef060bf28d82ec6f45c61c9002d7c7
Dimitrios Siganos <dimitris@siganos.org>
2016-04-22 15:13:03 +0100

    Add msd45n-binaries to the build dependencies of sdcts

--- package/sdcts/sdcts.mk
+++ package/sdcts/sdcts.mk
@@ -2,6 +2,6 @@ SDCTS_VERSION = custom
 SDCTS_SITE = $(TOPDIR)/../../sdcts
 SDCTS_SITE_METHOD = local
 SDCTS_AUTORECONF = YES
-#SDCTS_SITE_DEPENDENCIES =
+SDCTS_DEPENDENCIES = msd45n-binaries
 
 $(eval $(autotools-package))
